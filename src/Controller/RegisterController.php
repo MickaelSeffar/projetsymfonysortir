d54ecf1c7b18b2284ecac98a7e61ab3f100526cc
MickaelSeffar <mickael.seffar@gmail.com>
2021-04-09 15:50:43 +0200

    Inscription à une activité : je suis nul

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -1,9 +1,14 @@
 <?php
 
 
-
 namespace App\Controller;
 
+use App\Entity\Activity;
+use App\Entity\Register;
+use Doctrine\DBAL\Schema\Visitor\AbstractVisitor;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -11,7 +16,45 @@ use Symfony\Component\Routing\Annotation\Route;
  * @package App\Controller
  * @Route(path="inscription2/", name="register_")
  */
-class RegisterController
+class RegisterController extends AbstractController
 {
+    /**
+     * @Route (path="{activityId}", name="activityRegistration")
+     */
+    public function activityRegistration(EntityManagerInterface $entityManager, $activityId,$userId)
+    {
 
+        //$id = $request->get('id');
+        $activity = $entityManager->getRepository('App:Activity')->find($activityId);
+        $user = $entityManager->getRepository('App:User')->find($userId);
+       // $userconnecte = $this->getUser();
+
+        $register = new Register();
+        $register->setUser($user);
+        $register->setActivity($activity);
+
+        $register->setRegisterDate(new \DateTime());
+
+        $register->setActive(true);
+
+        $entityManager->persist($activity);
+        $entityManager->persist($register);
+
+        $entityManager->flush();
+
+
+        return $this->redirectToRoute("activity_detail", ['id' => $activity->getId()]);
+
+        //récupérer le nombre d'inscrit
+        /* $nbinscrit = $activity->getCurrentUserNumber();
+         //Récupérer le nombre maximum d'utilisateur
+         $maxinscrit = $activity->getMaximumUserNumber();
+
+         if ($nbinscrit<$maxinscrit){
+
+         }
+        */
+
+
+    }
 }
