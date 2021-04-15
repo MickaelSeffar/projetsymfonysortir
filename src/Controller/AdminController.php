95d8e7f20bb7d0beb2672dcfface220f49eb3917
IRVOASPAUL <irvoaspaul@outlook.fr>
2021-04-15 16:41:17 +0200

    Paul :   - CSS Accueil

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -36,27 +36,21 @@ class AdminController extends AbstractController
 
             if ($csvFieldAdmin) {
                 $originalFilename = pathinfo($csvFieldAdmin->getClientOriginalName(), PATHINFO_FILENAME);
-                // this is needed to safely include the file name as part of the URL
                 $newFilename = 'DATA.csv';
 
-                // Move the file to the directory where brochures are stored
                 try {
                     $csvFieldAdmin->move(
                         $this->getParameter('csv_directory'),
                         $newFilename
                     );
                 } catch (FileException $e) {
-                    // ... handle exception if something happens during file upload
                 }
 
-                // updates the 'brochureFilename' property to store the PDF file name
-                // instead of its contents
                 $user->setCsvFieldAdmin($newFilename);
             }
             $this->addFlash('success', 'Le fichier CSV a bien été envoyé');
             return $this->redirectToRoute('home_welcome');
         }
-        $this->addFlash('error', 'Erreur de l\'envoi du fichier csv');
         return $this->render('admin/addusercsv.html.twig', ['formAdmin' => $form->createView()]);
     }
 
